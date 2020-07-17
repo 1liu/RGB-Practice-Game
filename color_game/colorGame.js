e7413f730b860a9a0d3e8496ebd4c6c09c4d7485
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-17 11:32:08 -0700

    [color game] able to set num of squares

--- color_game/colorGame.js
+++ color_game/colorGame.js
@@ -1,19 +1,19 @@
-const colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-];
-
-const squares = document.querySelectorAll(".square");
+const numOfSquare = 6;
+const colors = generateColorArray(numOfSquare);
+const container = document.querySelector(".container");
 const targetColorEl = document.getElementById("targetColor");
 const messageEl = document.getElementById("message")
-const targetColor = colors[0];
+const targetColor = randomColor();
 targetColorEl.textContent = targetColor;
 
-for (let i = 0; i < squares.length; i++) {
+for (let i = 0; i < numOfSquare; i++) {
+  let square = document.createElement("div");
+  square.className = "square";
+  container.appendChild(square);
+}
+
+const squares = document.querySelectorAll(".square");
+for (let i = 0; i < numOfSquare; i++) {
   squares[i].style.backgroundColor = colors[i];
   squares[i].addEventListener("click", check);
 }
@@ -25,7 +25,7 @@ function check(event) {
     changeAllColor(targetColor);
     messageEl.textContent = "Congratulation! You picked the correct one.";
   } else {
-    messageEl.textContent = "Try Again"
+    messageEl.textContent = `This is ${this.style.backgroundColor}, try again`;
     this.style.backgroundColor = "#232323";
   }
 }
@@ -40,4 +40,21 @@ function changeAllColor(color) {
   for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = color;
   }
+}
+
+function randomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
+
+function generateColorArray(n) {
+  let array = [];
+  for (let i = 0; i < n; i++) {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    array.push(`rgb(${r}, ${g}, ${b})`);
+  }
+  console.log(array);
+  return array;
 }
